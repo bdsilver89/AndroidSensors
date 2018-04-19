92a7d051b0dfc6beb9d698ee3e2828177c1d080a
bdsilver89 <github@briansilver.net>
2018-04-19 18:54:37 -0400

    Created functional pedometer app

--- SimplePedometer/app/src/main/java/com/example/simplepedometer/MainActivity.java
+++ SimplePedometer/app/src/main/java/com/example/simplepedometer/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.simplepedometer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv_steps;
+
+    SensorManager sensorManager;
+
+    boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_steps = (TextView) findViewById(R.id.tv_steps);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null){
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        else {
+            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        running = false;
+        //unregister the sensor to stop counting
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if(running) {
+            tv_steps.setText(String.valueOf((int)event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
